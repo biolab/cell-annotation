6c255141498bc28a66398758aedc0dc9715d101c
Primož Godec <p.godec9@gmail.com>
2019-10-01 15:48:52 +0200

    Added new input parameters, renamed num_genes parameters and set default value

--- pointannotator/annotate_samples.py
+++ pointannotator/annotate_samples.py
@@ -22,22 +22,22 @@ class ScoringNotImplemented(Exception):
 class AnnotateSamples:
     """
     AnnotateSamples is class used for the annotation of data items with the
-    labels (e.g. Cell Types) Mann-Whitney U test for selecting important values
-    and the Hyper-geometric for assigning the labels.
+    annotations (e.g. Cell Types). We used the Mann-Whitney U test for selecting
+    important values and the Hyper-geometric for assigning the annotations.
 
-    Example for full annotation:
+    Example on biological data where we assign cell types to cells:
 
     >>> gene_expressions_df = pd.read_csv("data/DC_expMatrix_DCnMono.csv.gz",
     ...                                   compression='gzip')
     >>> marker_genes_df = pd.read_csv("data/panglao_gene_markers.csv.gz",
     ...                               compression="gzip")
     >>> # rename genes column and filter human markers
-    >>> marker_genes_df = marker_genes_df.rename(columns={'Name': 'Gene'})
     >>> marker_genes_df = marker_genes_df[
     ...     marker_genes_df["Organism"] == "Human"]
     >>>
     >>> annotations = AnnotateSamples.annotate_samples(
-    ...     gene_expressions_df, marker_genes_df, num_genes=60000,
+    ...     gene_expressions_df, marker_genes_df, num_all_attributes=60000,
+    ...     attributes_col="Cell Type", annotations_col="Name",
     ...     p_threshold=0.05)
 
     Example for full manual annotation. Here annotation is split in three
@@ -45,28 +45,25 @@ class AnnotateSamples:
 
     >>> z = AnnotateSamples.mann_whitney_test(gene_expressions_df)
     >>> scores, p_val = AnnotateSamples.assign_annotations(
-    ...     z, marker_genes_df, gene_expressions_df, num_genes=60000)
+    ...     z, marker_genes_df, gene_expressions_df, num_all_attributes=60000,
+    ...     attributes_col="Cell Type", annotations_col="Name")
     >>> scores = AnnotateSamples.filter_annotations(
     ...     scores, p_val, p_threshold=0.05)
-
-    Attributes
-    ----------
-
     """
     @staticmethod
     def log_cpm(data):
         """
-        Function normalizes data with CPM methods and normalize them.
+        Function normalizes data with the log CPM methods.
 
         Parameters
         ----------
         data : pd.DataFrame
-            Non-normalized gene expression data.
+            Non-normalized data table.
 
         Returns
         -------
-        Orange.data.Table
-            Normalized gene expression data
+        pd.DataFrame
+            Normalized data table.
         """
         norm_data = np.log(1 + AnnotateSamples._cpm(data))
         return norm_data
@@ -78,8 +75,8 @@ class AnnotateSamples:
 
         Parameters
         ----------
-        data : array_like
-            Numpy array with data. Columns are genes, rows are cells.
+        data : pd.DataFrame
+            Tabular data.
         """
         return data.divide(np.sum(data, axis=1)[:, None]) * 1e6
 
@@ -116,12 +113,12 @@ class AnnotateSamples:
     @staticmethod
     def mann_whitney_test(data):
         """
-        Compute z values with test Mann-Whitney U test.
+        Compute z values with the Mann-Whitney U test.
 
         Parameters
         ----------
         data : pd.DataFrame
-            Tabular data with gene expressions
+            Tabular data.
 
         Returns
         -------
@@ -158,16 +155,16 @@ class AnnotateSamples:
         return z_dataframe
 
     @staticmethod
-    def _reorder_matrix(matrix, genes_order):
+    def _reorder_matrix(matrix, attributes_order):
         """
-        Function reorder the columns of the array to fit to the genes_order
+        Function reorder the columns of the array to fit the attributes_order.
 
         Parameters
         ----------
         matrix : pd.DataFrame
-            Tabular data tha needs to be reordered
-        genes_order : list
-            Desired genes order
+            Tabular data that needs to be reordered
+        attributes_order : list
+            Desired attributes order
 
         Returns
         ------
@@ -176,29 +173,29 @@ class AnnotateSamples:
         """
         current_order = np.array(matrix.columns.values)
         values = matrix.values
-        genes_order = np.array(genes_order)
+        attributes_order = np.array(attributes_order)
 
-        xsorted = np.argsort(genes_order)
-        ypos = np.searchsorted(genes_order[xsorted], current_order)
+        xsorted = np.argsort(attributes_order)
+        ypos = np.searchsorted(attributes_order[xsorted], current_order)
         indices = xsorted[ypos]  # index which tell where should be the column
 
-        reordered_values = np.zeros((values.shape[0], len(genes_order)))
+        reordered_values = np.zeros((values.shape[0], len(attributes_order)))
         for i_curr, i_dest in enumerate(indices):
             reordered_values[:, i_dest] = values[:, i_curr]
 
         return reordered_values
 
     @staticmethod
-    def _select_attributes(z, genes_order, z_threshold=1):
+    def _select_attributes(z, attributes_order, z_threshold=1):
         """
-        Function selects "over"-expressed attributes for items based on z
-        values. It also reorder the matrix columns.
+        The function selects "over"-expressed attributes for items based on z
+        values. It also reorders the matrix columns.
 
         Parameters
         ----------
         z : pd.Dataframe
             Tabular data z values for each item in the table
-        genes_order : list
+        attributes_order : list
             Desired genes order
         z_threshold : float
             The threshold for selecting the attribute. For each item the
@@ -207,33 +204,37 @@ class AnnotateSamples:
         Returns
         -------
         np.ndarray
-            Reordered and thresholded z-values/
+            Reordered and thresholded z-values
         """
-        reordered_z = AnnotateSamples._reorder_matrix(z, genes_order)
+        reordered_z = AnnotateSamples._reorder_matrix(z, attributes_order)
 
         return reordered_z > z_threshold
 
     @staticmethod
-    def _group_marker_attributes(markers, genes_order):
+    def _group_attributes_annotations(available_annotations, attributes_order,
+                                      attributes_col, annotations_col):
         """
-        Function transforms annotations to
-        matrix with size (genes_order x types)
+        Function transforms annotations to matrix with the size
+        (attributes_order x annotations).
         """
-        types = sorted(list(set(markers.loc[:, "Cell Type"].values)))
-        genes_celltypes = np.zeros((len(genes_order), len(types)))
+        types = sorted(
+            list(set(available_annotations.loc[:, annotations_col].values)))
+        attributes_annotations = np.zeros((len(attributes_order), len(types)))
 
-        for _, m in markers.iterrows():
-            g = m["Gene"]
-            m = m["Cell Type"]
+        for _, m in available_annotations.iterrows():
+            g = m[attributes_col]
+            m = m[annotations_col]
             if g is not None:
-                genes_celltypes[genes_order.index(g), types.index(m)] = 1
+                attributes_annotations[
+                    attributes_order.index(g), types.index(m)] = 1
 
-        return genes_celltypes, types
+        return attributes_annotations, types
 
     @staticmethod
-    def _score(scoring_type, p_values, fdrs, data, M, x, m, genes_order):
+    def _score(scoring_type, p_values, fdrs, data, M, x, m, attributes_order):
         if scoring_type == SCORING_MARKERS_SUM:
-            return AnnotateSamples._reorder_matrix(data, genes_order).dot(M)
+            return AnnotateSamples._reorder_matrix(
+                data, attributes_order).dot(M)
         elif scoring_type == SCORING_EXP_RATIO:
             return x / m
         elif scoring_type == SCORING_LOG_FDR:
@@ -244,33 +245,50 @@ class AnnotateSamples:
             raise ScoringNotImplemented()
 
     @staticmethod
-    def assign_annotations(z_values, available_annotations, data, num_genes,
+    def assign_annotations(z_values, available_annotations, data,
+                           num_all_attributes=None,
+                           attributes_col="Attributes",
+                           annotations_col="Annotations",
                            z_threshold=1,
                            p_value_fun=PFUN_BINOMIAL,
                            scoring=SCORING_EXP_RATIO):
         """
-        The function gets a set of attributes (e.g. genes) for each cell and
+        The function gets a set of attributes (e.g. genes) for each item and
         attributes for each annotation. It returns the annotations significant
-        for each cell.
+        for each item.
 
         Parameters
         ----------
         z_values : pd.DataFrame
-            DataFrame which show z values for each item
+            DataFrame that shows z values for each item. Rows are data items
+            and columns are attributes.
         available_annotations : pd.DataFrame
             Available annotations (e.g. cell types), this data frame has
-            two columns `Genes` and `Cell Types`.
-        num_genes : int
-            Number of genes that the organism has.
+            two columns: attributes column name is set by *attributes_col*
+            variable (default: Attributes) and annotations is set by
+            *annotations_col* variable (default: Annotations).
+        data : pd.DataFrame
+            Tabular input (raw) data - we need that to compute
+            scores.
+        num_all_attributes : int
+            The number of all attributes for a case (also those that do not
+            appear in the data). In the case of genes, it is the number of all
+            genes that an organism has. It is recommended to set your value,
+            in cases when the value is not set the number of attributes in
+            z_values table will be used.
+        attributes_col : str
+            The name of an attributes column in available_annotations
+            (default: Attributes").
+        annotations_col : str
+            The name of an annotations column in available_annotations
+            (default: Annotations").
         z_threshold : float
-            The threshold for selecting the attribute. For each item the
+            The threshold for selecting the attribute. For each item, the
             attributes with z-value above this value are selected.
         p_value_fun : str, optional (defaults: TEST_BINOMIAL)
-            A function that calculates p-value. It can be either
+            A function that calculates the p-value. It can be either
             PFUN_BINOMIAL that uses binom.sf or
             PFUN_HYPERGEOMETRIC that uses hypergeom.sf.
-        data : pd.DataFrame
-            Tabular data with gene expressions - we need that to compute scores.
         scoring : str, optional (default=SCORING_EXP_RATIO)
             Type of scoring
 
@@ -279,7 +297,7 @@ class AnnotateSamples:
         pd.DataFrame
             Annotation probabilities
         pd.DataFrame
-            Annotation fdrs
+            Annotation FDRS.
         """
         if not isinstance(z_values, pd.DataFrame):
             raise TypeError("z_values argument must be pandas DataFrame")
@@ -288,10 +306,11 @@ class AnnotateSamples:
                             "DataFrame")
         if not isinstance(data, pd.DataFrame):
             raise TypeError("data argument must be pandas DataFrame")
-        if not available_annotations["Gene"].dtype == object:
-            raise TypeError("The type of genes column must be string/object")
-        if not available_annotations["Cell Type"].dtype == object:
-            raise TypeError("The type of cell type column must be "
+        if not available_annotations[attributes_col].dtype == object:
+            raise TypeError("The type of attributes_col column must be "
+                            "string/object")
+        if not available_annotations[annotations_col].dtype == object:
+            raise TypeError("The type of annotations_col column must be "
                             "string/object")
 
         # select function for p-value
@@ -301,29 +320,35 @@ class AnnotateSamples:
             p_fun = lambda x, N, m, k: binom.sf(x, k, m / N)
 
         # make an attributes order
-        genes_data = z_values.columns.values
-        genes_celltypes = available_annotations["Gene"].values
-        genes_order = list(set(genes_data) | set(genes_celltypes))
+        attributes = z_values.columns.values
+        attributes_annotations = available_annotations[attributes_col].values
+        attributes_order = list(set(attributes) | set(attributes_annotations))
 
-        # get marker genes matrix M
-        M, annotations = AnnotateSamples._group_marker_attributes(
-            available_annotations, genes_order)
+        # get attributes-annotations matrix M
+        M, annotations = AnnotateSamples._group_attributes_annotations(
+            available_annotations, attributes_order,
+            attributes_col, annotations_col)
 
         Z = AnnotateSamples._select_attributes(
-            z_values, genes_order, z_threshold)
+            z_values, attributes_order, z_threshold)
+
+        # if user do not set own num_all_attributes value it is set to number
+        # of attributes in the table
+        if num_all_attributes is None:
+            num_all_attributes = len(z_values.columns)
 
         x = Z.dot(M)
         k = np.repeat(Z.sum(axis=1).reshape(-1, 1), x.shape[1], axis=1)
         m = np.repeat(M.sum(axis=0).reshape(1, -1), x.shape[0], axis=0)
 
-        p_values = p_fun(x - 1, num_genes, m, k)
+        p_values = p_fun(x - 1, num_all_attributes, m, k)
 
         fdrs = np.zeros(p_values.shape)
         for i, row in enumerate(p_values):
             fdrs[i] = np.array(FDR(row.tolist()))
 
         scores = AnnotateSamples._score(
-            scoring, p_values, fdrs, data, M, x, m, genes_order)
+            scoring, p_values, fdrs, data, M, x, m, attributes_order)
 
         scores_table = pd.DataFrame(
             scores, columns=annotations, index=data.index)
@@ -336,25 +361,25 @@ class AnnotateSamples:
                            p_threshold=0.05):
         """
         This function filters the probabilities on places that do not reach the
-        threshold for p-value and filter zero columns
+        threshold for p-value and filter zero columns if
         return_nonzero_annotations is True.
 
         Parameters
         ----------
         scores : pd.DataFrame
-            Scores for each annotations for each cell
+            Scores for each annotation for data items
         p_values : pd.DataFrame
-            p-value scores for annotations for each cell
+            p-value scores for annotations for data items
         return_nonzero_annotations : bool
-            Flag that enables filtering the non-zero columns.
+            The flag that enables filtering the non-zero columns.
         p_threshold : float
-            A threshold for accepting the annotations. Annotations that has FDR
-            value bellow this threshold are used.
+            A threshold for accepting the annotations. Annotations that have
+            FDR value bellow this threshold are used.
 
         Returns
         -------
         pd.Dataframe
-            Filtered scores for each annotations for each cell
+            Filtered scores for each annotation for data items
         """
         if not isinstance(scores, pd.DataFrame):
             raise TypeError("scores argument must be pandas DataFrame")
@@ -370,26 +395,43 @@ class AnnotateSamples:
         return scores
 
     @staticmethod
-    def annotate_samples(data, available_annotations, num_genes,
+    def annotate_samples(data, available_annotations,
+                         num_all_attributes=None,
+                         attributes_col="Attributes",
+                         annotations_col="Annotations",
                          return_nonzero_annotations=True, p_threshold=0.05,
                          p_value_fun=PFUN_BINOMIAL, z_threshold=1,
                          scoring=SCORING_EXP_RATIO, normalize=False):
         """
         Function marks the data with annotations that are provided. This
-        function implements the complete functionality. First select genes,
-        then annotate them and filter them.
+        function implements the complete functionality. First select
+        attributes for each item with z_test, then annotate data and
+        filter data.
 
         Parameters
         ----------
         data : pd.DataFrame
             Tabular data
         available_annotations : pd.DataFrame
-            Available annotations (e.g. cell types)
-        num_genes : int
-            Number of genes that the organism has.
+            Available annotations (e.g. cell types), this data frame has
+            two columns: attributes column name is set by *attributes_col*
+            variable (default: Attributes) and annotations is set by
+            *annotations_col* variable (default: Annotations).
+        num_all_attributes : int
+            The number of all attributes for a case (also those that do not
+            appear in the data). In the case of genes, it is the number of all
+            genes that an organism has. It is recommended to set your value,
+            in cases when the value is not set the number of attributes in
+            z_values table will be used.
         return_nonzero_annotations : bool, optional (default=True)
             If true return scores for only annotations present in at least one
             sample.
+        attributes_col : str
+            The name of an attributes column in available_annotations
+            (default: Attributes").
+        annotations_col : str
+            The name of an annotations column in available_annotations
+            (default: Annotations").
         p_threshold : float
             A threshold for accepting the annotations. Annotations that has FDR
             value bellow this threshold are used.
@@ -408,7 +450,8 @@ class AnnotateSamples:
         Returns
         -------
         pd.DataFrame
-            Cell type most important for each cell.
+            Scores table - each line of the table has scores that tell how
+            probable is that items have specific annotations.
         """
         if not isinstance(data, pd.DataFrame):
             raise TypeError("data argument must be pandas DataFrame")
@@ -426,7 +469,10 @@ class AnnotateSamples:
             data)
 
         annotation_probs, annotation_fdrs = AnnotateSamples.assign_annotations(
-            z, available_annotations, data, num_genes, z_threshold=z_threshold,
+            z, available_annotations, data,
+            num_all_attributes=num_all_attributes,
+            attributes_col=attributes_col, annotations_col=annotations_col,
+            z_threshold=z_threshold,
             p_value_fun=p_value_fun, scoring=scoring)
 
         annotation_probs = AnnotateSamples.filter_annotations(
